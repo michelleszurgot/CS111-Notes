d2d6001835672848071916e262c4156278cfe2ac
Michelle Szurgot <michelleszurgot@u.boisestate.edu>
2019-09-25 17:13:08 -0600

    added a file

---
+++ SalaryConverter.py
@@ -0,0 +1,12 @@
+def askUserSalary():
+    annualSalary = int(input("What is your annual salary?"))
+    return annualSalary
+
+def convertAnnualSalary(annualSalary)
+    weeklyRate = annualSalary / 50
+    hourlyRate = weeklyRate / 40
+    return hourlyRate
+
+salary = askUserSalary(salary)
+
+print("Your hourly rate is " +str(usersHourlyRate))
